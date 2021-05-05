099b804731c90bb902a01e35aec47daca2b81d84
Christopher Watford <christopherw@suzy.com>
2021-05-05 10:26:34 -0400

    Add console controller for CodeGenerator

---
+++ Generation/Tools/CodeGenerator/CodeGeneratorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace CodeGenerator
+{
+    public class CodeGeneratorController
+    {
+        private readonly Translator _translator;
+
+        public CodeGeneratorController(Translator translator)
+        {
+            _translator = translator ?? throw new ArgumentNullException(nameof(translator));
+        }
+
+        public void LoadOptions(string path)
+        {
+            Options options;
+
+            try
+            {
+                options = Utility.DeserializeFromXml<Options>(new FileInfo(path));
+            }
+            catch (FileNotFoundException)
+            {
+                return;
+            }
+
+            if (options == null)
+            {
+                return;
+            }
+
+            _translator.DataFile = options.DataFile;
+            _translator.OutputFolder = options.OutputFolder;
+
+            foreach (string selectedFile in options.SelectedFiles)
+            {
+                _translator.Templates.Add(new Template(selectedFile, selectedFile, isSelected: true));
+            }
+
+            foreach (string unSelectedFile in options.UnSelectedFiles)
+            {
+                _translator.Templates.Add(new Template(unSelectedFile, unSelectedFile, isSelected: false));
+            }
+        }
+
+        public void GenerateFiles()
+            => _translator.GenFiles();
+    }
+}
--- Generation/Tools/CodeGenerator/Program.cs
+++ Generation/Tools/CodeGenerator/Program.cs
@@ -1,16 +1,58 @@
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CodeGenerator
 {
-	internal static class Program
-	{
-		[STAThread]
-		private static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
-			Application.Run(new PrimaryForm());
-		}
-	}
+    public static class Program
+    {
+        public static TextWriter StandardOutput { get; set; } = Console.Out;
+
+        [STAThread]
+        private static int Main(string[] args)
+        {
+            if (args?.Length > 0)
+            {
+                return RunConsole(args);
+            }
+            else
+            {
+                return RunApp();
+            }
+        }
+
+        private static int RunApp()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+            Application.Run(new PrimaryForm());
+
+            return Environment.ExitCode;
+        }
+
+        private static int RunConsole(string[] args)
+        {
+            Environment.ExitCode = 1;
+            try
+            {
+                var translator = new Translator();
+                var controller = new CodeGeneratorController(translator);
+
+                translator.Notice += (sender, e) => StandardOutput.WriteLine($"[INFO] {e.Message}");
+                translator.Error += (sender, e) => StandardOutput.WriteLine($"[ERROR] {e.Message}");
+                translator.Progress += (sender, e) => StandardOutput.WriteLine($"[INFO] {e.Progress:0.0} % complete."); ;
+
+                controller.LoadOptions(args[0]);
+                controller.GenerateFiles();
+
+                Environment.ExitCode = 0;
+            }
+            catch (Exception ex)
+            {
+                StandardOutput.WriteLine($"[ERROR] {ex}");
+            }
+
+            return Environment.ExitCode;
+        }
+    }
 }
