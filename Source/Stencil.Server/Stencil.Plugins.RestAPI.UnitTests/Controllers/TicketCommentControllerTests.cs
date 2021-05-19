27e463429fae64f0bea9e7be82a07df2350e86fb
Christopher Watford <christopherw@suzy.com>
2021-05-19 14:00:50 -0400

    Add business logic and security logic for TicketComments

---
+++ Source/Stencil.Server/Stencil.Plugins.RestAPI.UnitTests/Controllers/TicketCommentControllerTests.cs
@@ -0,0 +1,298 @@
+﻿using Microsoft.Practices.Unity;
+using Moq;
+using Stencil.Primary;
+using Stencil.Primary.Business.Direct;
+using Stencil.Primary.Business.Index;
+using Stencil.SDK;
+using System;
+using System.Web.Http;
+using Xunit;
+
+using dm = Stencil.Domain;
+using sdk = Stencil.SDK.Models;
+
+namespace Stencil.Plugins.RestAPI.Controllers
+{
+    public class TicketCommentControllerTests : ControllerTestBase<TicketCommentController>
+    {
+        protected override TicketCommentController CreateController() => new(_foundation.Object);
+
+        [Fact]
+        public void Create_Sets_Commenter_To_Current_Account()
+        {
+            DateTime now = DateTime.UtcNow;
+
+            dm.TicketComment insertedTicketComment = null;
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.Insert(It.IsAny<dm.TicketComment>()))
+                          .Returns<dm.TicketComment>(tt =>
+                          {
+                              // Capture the inserted ticket
+                              insertedTicketComment = tt;
+                              tt.ticket_comment_id = Guid.NewGuid();
+                              return tt;
+                          });
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+            var ticketCommentIndex = new Mock<ITicketCommentIndex>();
+            ticketCommentIndex.Setup(tt => tt.GetById(It.IsAny<Guid>()))
+                       .Returns<Guid>(id =>
+                       {
+                           Assert.NotNull(insertedTicketComment);
+                           Assert.Equal(insertedTicketComment.ticket_comment_id, id);
+
+                           return insertedTicketComment.ToSDKModel();
+                       });
+            _container.RegisterInstance<ITicketCommentIndex>(ticketCommentIndex.Object);
+
+            var ticketCommentController = CreatePost("ticketcomments", _userAccount);
+
+            var ticketComment = new sdk.TicketComment
+            {
+                ticket_id = Guid.NewGuid(),
+                ticket_comment ="Hello World!",
+            };
+
+            var response = ticketCommentController.Create(ticketComment);
+
+            var httpResponse = HttpAssert.IsCreatedResponse(response);
+
+            var itemResult = HttpAssert.IsContent<ItemResult<sdk.TicketComment>>(httpResponse.Content);
+            Assert.True(itemResult.success);
+            Assert.NotNull(itemResult.item);
+            Assert.NotNull(insertedTicketComment);
+            Assert.Equal(insertedTicketComment.ticket_id, itemResult.item.ticket_id);
+            Assert.Equal(insertedTicketComment.ticket_comment, itemResult.item.ticket_comment);
+            Assert.Equal(_userAccount.account_id, itemResult.item.commenter_id);
+            Assert.True(now <= itemResult.item.commented_on_utc);
+        }
+
+
+        [Fact]
+        public void Update_Allows_Account_If_Can_Update()
+        {
+            dm.TicketComment updatedTicket = null;
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountUpdateTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(true);
+            ticketCommentBusiness.Setup(tt => tt.Update(It.IsAny<dm.TicketComment>()))
+                          .Returns<dm.TicketComment>(tt =>
+                          {
+                              // Capture the updated ticket
+                              updatedTicket = tt;
+                              return tt;
+                          });
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            var ticketIndex = new Mock<ITicketCommentIndex>();
+            ticketIndex.Setup(tt => tt.GetById(It.IsAny<Guid>()))
+                       .Returns<Guid>(id =>
+                       {
+                           Assert.NotNull(updatedTicket);
+                           Assert.Equal(updatedTicket.ticket_comment_id, id);
+
+                           return updatedTicket.ToSDKModel();
+                       });
+            _container.RegisterInstance<ITicketCommentIndex>(ticketIndex.Object);
+
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketComment = new sdk.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = _userAccount.account_id,
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+
+            var ticketCommentController = CreatePut($"ticketcomments/{ticket_comment_id}", _userAccount);
+
+            var response = ticketCommentController.Update(ticket_comment_id, ticketComment);
+
+            var httpResponse = HttpAssert.IsSuccess(response);
+
+            var itemResult = HttpAssert.IsContent<ItemResult<sdk.TicketComment>>(httpResponse.Content);
+            Assert.True(itemResult.success);
+            Assert.NotNull(itemResult.item);
+            Assert.NotNull(updatedTicket);
+            Assert.Equal(updatedTicket.ticket_comment_id, itemResult.item.ticket_comment_id);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountUpdateTicketComment(It.Is<dm.Account>(aa => aa.account_id == _userAccount.account_id), ticket_comment_id), Times.Once());
+        }
+
+        [Fact]
+        public void Update_Allows_Admin_Account_If_Can_Cannot_Update()
+        {
+            dm.TicketComment updatedTicket = null;
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountUpdateTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(false);
+            ticketCommentBusiness.Setup(tt => tt.Update(It.IsAny<dm.TicketComment>()))
+                          .Returns<dm.TicketComment>(tt =>
+                          {
+                              // Capture the updated ticket
+                              updatedTicket = tt;
+                              return tt;
+                          });
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            var ticketIndex = new Mock<ITicketCommentIndex>();
+            ticketIndex.Setup(tt => tt.GetById(It.IsAny<Guid>()))
+                       .Returns<Guid>(id =>
+                       {
+                           Assert.NotNull(updatedTicket);
+                           Assert.Equal(updatedTicket.ticket_comment_id, id);
+
+                           return updatedTicket.ToSDKModel();
+                       });
+            _container.RegisterInstance<ITicketCommentIndex>(ticketIndex.Object);
+
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketComment = new sdk.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = Guid.NewGuid(),
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+
+            var ticketCommentController = CreatePut($"ticketcomments/{ticket_comment_id}", _adminAccount);
+
+            var response = ticketCommentController.Update(ticket_comment_id, ticketComment);
+
+            var httpResponse = HttpAssert.IsSuccess(response);
+
+            var itemResult = HttpAssert.IsContent<ItemResult<sdk.TicketComment>>(httpResponse.Content);
+            Assert.True(itemResult.success);
+            Assert.NotNull(itemResult.item);
+            Assert.NotNull(updatedTicket);
+            Assert.Equal(updatedTicket.ticket_comment_id, itemResult.item.ticket_comment_id);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountUpdateTicketComment(It.Is<dm.Account>(aa => aa.account_id == _adminAccount.account_id), ticket_comment_id), Times.Once());
+        }
+
+        [Fact]
+        public void Update_Rejects_NonAdmin_Account_If_Cannot_Update()
+        {
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountUpdateTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(false);
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketComment = new sdk.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = _userAccount.account_id,
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+
+            var ticketCommentController = CreatePut($"ticketcomments/{ticket_comment_id}", _userAccount);
+
+            var exception = Assert.Throws<HttpResponseException>(() => ticketCommentController.Update(ticket_comment_id, ticketComment));
+
+            _ = HttpAssert.IsUnauthorizedResponse(exception.Response);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountUpdateTicketComment(It.Is<dm.Account>(aa => aa.account_id == _userAccount.account_id), ticket_comment_id), Times.Once());
+        }
+
+        [Fact]
+        public void Delete_Allows_Account_If_Can_Delete()
+        {
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountDeleteTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(true);
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            var ticketComment = new dm.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = _userAccount.account_id,
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+            ticketCommentBusiness.Setup(tt => tt.GetById(ticket_comment_id))
+                          .Returns<Guid>(id => ticketComment);
+
+            var ticketCommentController = CreateDelete($"ticketcomments/{ticket_comment_id}", _userAccount);
+
+            var response = ticketCommentController.Delete(ticket_comment_id);
+
+            var httpResponse = HttpAssert.IsSuccess(response);
+
+            var actionResult = HttpAssert.IsActionResult(httpResponse.Content);
+            Assert.True(actionResult.success);
+            Assert.NotNull(actionResult.message);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountDeleteTicketComment(It.Is<dm.Account>(aa => aa.account_id == _userAccount.account_id), ticket_comment_id), Times.Once());
+            ticketCommentBusiness.Verify(tt => tt.Delete(ticket_comment_id), Times.Once());
+        }
+
+        [Fact]
+        public void Delete_Allows_Admin_Account_If_Can_Cannot_Delete()
+        {
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountDeleteTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(false);
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            var ticketComment = new dm.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = _userAccount.account_id,
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+            ticketCommentBusiness.Setup(tt => tt.GetById(ticket_comment_id))
+                          .Returns<Guid>(id => ticketComment);
+
+            var ticketCommentController = CreateDelete($"ticketcomments/{ticket_comment_id}", _adminAccount);
+
+            var response = ticketCommentController.Delete(ticket_comment_id);
+
+            var httpResponse = HttpAssert.IsSuccess(response);
+
+            var actionResult = HttpAssert.IsActionResult(httpResponse.Content);
+            Assert.True(actionResult.success);
+            Assert.NotNull(actionResult.message);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountDeleteTicketComment(It.Is<dm.Account>(aa => aa.account_id == _adminAccount.account_id), ticket_comment_id), Times.Once());
+            ticketCommentBusiness.Verify(tt => tt.Delete(ticket_comment_id), Times.Once());
+        }
+
+        [Fact]
+        public void Delete_Rejects_NonAdmin_Account_If_Cannot_Delete()
+        {
+            Guid ticket_comment_id = Guid.NewGuid();
+            var ticketCommentBusiness = new Mock<ITicketCommentBusiness>();
+            ticketCommentBusiness.Setup(tt => tt.CanAccountDeleteTicketComment(It.IsAny<dm.Account>(), It.IsAny<Guid>()))
+                          .Returns(false);
+            _container.RegisterInstance<ITicketCommentBusiness>(ticketCommentBusiness.Object);
+
+            var ticketComment = new dm.TicketComment
+            {
+                ticket_comment_id = ticket_comment_id,
+                ticket_id = Guid.NewGuid(),
+                commenter_id = _userAccount.account_id,
+                commented_on_utc = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)),
+                ticket_comment = "Hello World!",
+            };
+            ticketCommentBusiness.Setup(tt => tt.GetById(ticket_comment_id))
+                          .Returns<Guid>(id => ticketComment);
+
+            var ticketCommentController = CreateDelete($"ticketcomments/{ticket_comment_id}", _userAccount);
+
+            var exception = Assert.Throws<HttpResponseException>(() => ticketCommentController.Delete(ticket_comment_id));
+
+            _ = HttpAssert.IsUnauthorizedResponse(exception.Response);
+
+            ticketCommentBusiness.Verify(tt => tt.CanAccountDeleteTicketComment(It.Is<dm.Account>(aa => aa.account_id == _userAccount.account_id), ticket_comment_id), Times.Once());
+        }
+    }
+}
---
+++ Source/Stencil.Server/Stencil.Primary.UnitTests/Business/Direct/Implementation/TicketCommentBusinessTests.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.Practices.Unity;
+using Moq;
+using Stencil.Data.Sql;
+using Stencil.Primary.Business.Index;
+using Stencil.Primary.Synchronization;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+using dm = Stencil.Domain;
+using sdk = Stencil.SDK.Models;
+
+namespace Stencil.Primary.Business.Direct.Implementation
+{
+    public class TicketCommentBusinessTests : BusinessTestBase
+    {
+        private readonly dm.Account _commenterAccount;
+        private readonly dm.Account _otherAccount;
+        private readonly dm.Ticket _ticket0;
+        private readonly dm.TicketComment _ticketComment0;
+
+        public TicketCommentBusinessTests()
+            : base()
+        {
+            _commenterAccount = new dm.Account
+            {
+                account_id = Guid.NewGuid(),
+            };
+            _otherAccount = new dm.Account
+            {
+                account_id = Guid.NewGuid(),
+            };
+            _ticket0 = new dm.Ticket
+            {
+                ticket_id = Guid.NewGuid(),
+                reported_by_id = _commenterAccount.account_id,
+                assigned_to_id = _otherAccount.account_id,
+            };
+            _ticketComment0 = new dm.TicketComment
+            {
+                ticket_comment_id = Guid.NewGuid(),
+                ticket_id = _ticket0.ticket_id,
+                commenter_id = _commenterAccount.account_id,
+            };
+
+            _context.dbAccounts.Add(new dbAccount
+            {
+                account_id = _commenterAccount.account_id,
+                email = "test0@example.com",
+                password = "password",
+                password_salt = "veruca",
+                api_key = "key",
+                api_secret = "secret",
+            });
+            _context.dbAccounts.Add(new dbAccount
+            {
+                account_id = _otherAccount.account_id,
+                email = "test1@example.com",
+                password = "password",
+                password_salt = "veruca",
+                api_key = "key",
+                api_secret = "secret",
+            });
+            _context.dbTickets.Add(new dbTicket
+            {
+                ticket_id = _ticket0.ticket_id,
+                reported_by_id = _ticket0.reported_by_id,
+                assigned_to_id = _ticket0.assigned_to_id,
+                ticket_title = "Title",
+                ticket_description = "Description",
+                opened_on_utc = DateTimeOffset.UtcNow,
+                ticket_type = (int)dm.TicketType.Feature,
+                ticket_status = (int)dm.TicketStatus.Open,
+            });
+            _context.dbTicketComments.Add(new dbTicketComment
+            {
+                ticket_comment_id = _ticketComment0.ticket_comment_id,
+                ticket_id = _ticketComment0.ticket_id,
+                commenter_id = _ticketComment0.commenter_id,
+                commented_on_utc = DateTimeOffset.UtcNow,
+                ticket_comment = "Hello World!",
+            });
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public void CanAccountUpdateTicketComment_Requires_Ticket_Comment()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(_commenterAccount, Guid.Empty));
+
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(_otherAccount, Guid.Empty));
+        }
+
+        [Fact]
+        public void CanAccountUpdateTicketComment_Requires_Account()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Guid ticket_comment_id = Guid.NewGuid();
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(null, ticket_comment_id));
+
+            dm.Account account = new dm.Account
+            {
+                account_id = Guid.Empty,
+            };
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(account, ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountUpdateTicketComment_Requires_Existing_Ticket_Comment()
+        {
+            Guid ticket_comment_id = Guid.NewGuid();
+
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(_commenterAccount, ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountUpdateTicketComment_Accepts_Ticket_Commenter()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.True(ticketCommentBusiness.CanAccountUpdateTicketComment(_commenterAccount, _ticketComment0.ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountUpdateTicketComment_Otherwise_Rejects_Account()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            var account = new dm.Account { account_id = Guid.NewGuid(), };
+
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(account, _ticketComment0.ticket_comment_id));
+
+            Assert.False(ticketCommentBusiness.CanAccountUpdateTicketComment(_otherAccount, _ticketComment0.ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountDeleteTicketComment_Requires_Ticket()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(_commenterAccount, Guid.Empty));
+
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(_otherAccount, Guid.Empty));
+        }
+
+        [Fact]
+        public void CanAccountDeleteTicketComment_Requires_Account()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Guid ticket_comment_id = Guid.NewGuid();
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(null, ticket_comment_id));
+
+            dm.Account account = new dm.Account
+            {
+                account_id = Guid.Empty,
+            };
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(account, ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountDeleteTicketComment_Requires_Existing_Ticket_Comment()
+        {
+            Guid ticket_comment_id = Guid.NewGuid();
+
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(_commenterAccount, ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountDeleteTicketComment_Accepts_Ticket_Commenter()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            Assert.True(ticketCommentBusiness.CanAccountDeleteTicketComment(_commenterAccount, _ticketComment0.ticket_comment_id));
+        }
+
+        [Fact]
+        public void CanAccountDeleteTicketComment_Otherwise_Rejects_Account()
+        {
+            var ticketCommentBusiness = new TicketCommentBusiness(_foundation.Object);
+
+            var account = new dm.Account { account_id = Guid.NewGuid(), };
+
+            Assert.False(ticketCommentBusiness.CanAccountDeleteTicketComment(account, _ticketComment0.ticket_comment_id));
+        }
+    }
+}
